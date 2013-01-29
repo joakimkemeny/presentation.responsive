2f13f1a0ac6e5b5daaa8f2ce87fadae6e488f7c0
Joakim Kemeny <joakim@kemeny.se>
2013-01-29 16:06:31 +0100

    Updated the script files.

--- demo/demo7/js/scripts.js
+++ demo/demo7/js/scripts.js
@@ -1,36 +1,27 @@
 // @codekit-prepend "../../js/libs/jquery.js"
 
-var $ = $ || {};
-var Responsive = Responsive || {};
-
-Responsive.Demo = (function ($) {
+(function () {
     "use strict";
-    var Demo = {};
 
-    Demo.createMenu = function () {
+    var Responsive = {
 
-        $(".menu").click(function () {
-
-            var aside = $("aside");
-            if (aside.css("left") === "-90%") {
-                aside.animate({
-                    left : "0%"
-                }, 1000);
-            } else {
-                aside.animate({
-                    left : "-90%"
-                }, 1000);
-            }
-
-            return false;
-        });
+        /**
+         * Attaches a click handler that toggles the side menu.
+         */
+        createMenu : function () {
+            $(".menu").click(function () {
+                var aside = $("aside");
+                if (aside.css("left") === "-90%") {
+                    aside.animate({ left : "0%" }, 1000);
+                } else {
+                    aside.animate({ left : "-90%" }, 1000);
+                }
+                return false;
+            });
+        }
     };
 
-
-    return Demo;
-
-}($));
-
-$(function () {
-    Responsive.Demo.createMenu();
-});
+    $(function () {
+        Responsive.createMenu();
+    });
+}());
--- js/scripts.js
+++ js/scripts.js
@@ -3,125 +3,122 @@
 // @codekit-prepend "libs/impress.js"
 // @codekit-prepend "libs/rainbow.js"
 // @codekit-prepend "libs/language/c.js"
+// @codekit-prepend "libs/language/coffeescript.js"
 // @codekit-prepend "libs/language/csharp.js"
 // @codekit-prepend "libs/language/css.js"
 // @codekit-prepend "libs/language/generic.js"
+// @codekit-prepend "libs/language/go.js"
+// @codekit-prepend "libs/language/haskell.js"
 // @codekit-prepend "libs/language/html.js"
+// @codekit-prepend "libs/language/java.js"
 // @codekit-prepend "libs/language/javascript.js"
 // @codekit-prepend "libs/language/lua.js"
 // @codekit-prepend "libs/language/php.js"
 // @codekit-prepend "libs/language/python.js"
+// @codekit-prepend "libs/language/r.js"
 // @codekit-prepend "libs/language/ruby.js"
 // @codekit-prepend "libs/language/scheme.js"
 // @codekit-prepend "libs/language/shell.js"
+// @codekit-prepend "libs/language/smalltalk.js"
 
-var $ = $ || {};
-var Responsive = Responsive || {};
-
-Responsive = (function ($) {
+(function () {
     "use strict";
-    var Presentation = {};
 
-    Presentation.loadDemo = function (step) {
-        if (step.data("demo-loaded")) {
-            return;
-        }
+    var Responsive = {
 
-        var browser = step.find(".browser");
-        var addressBar = browser.find(".address div");
-        browser.append("<iframe src='" + addressBar.text() + "' frameborder='0'></iframe>");
-
-        var iframe = browser.find("iframe");
-        iframe.load(function () {
-            addressBar.text(this.src);
-        });
-
-        step.data("demo-loaded", true);
-    };
-
-    Presentation.loadSrc = function (step) {
-        if (step.data("src-loaded")) {
-            return;
-        }
-
-        $("code[data-src]", step).each(function (index, element) {
-            var $element = $(element)
-            var srcUrl = $element.data("src");
-            var srcLang = $element.data("language");
-            $.get(srcUrl, function (data) {
-                if (srcLang === "text" || srcLang === "") {
-                    $element.append(data);
-                } else {
-                    Rainbow.color(data, srcLang, function (highlighted) {
-                        $element.append(highlighted);
-                    });
-                }
-            }, "text");
-        });
-
-        step.data("src-loaded", true);
-    };
-
-    Presentation.resizeBrowser = function () {
-        $(document).keypress(function (e) {
-            if (e.keyCode == 46) {
-                var $el;
-                if (document.location.hash == "#/demo-1") {
-                    $el = $('#demo-1 .browser');
-                } else if (document.location.hash == "#/demo-2") {
-                    $el = $('#demo-2 .browser');
-                } else if (document.location.hash == "#/demo-3") {
-                    $el = $('#demo-3 .browser');
-                } else if (document.location.hash == "#/grid-2") {
-                    $el = $('#grid-2 .browser');
-                } else if (document.location.hash == "#/grid-4") {
-                    $el = $('#grid-4 .browser');
-                } else if (document.location.hash == "#/images-3") {
-                    $el = $('#images-3 .browser');
-                } else if (document.location.hash == "#/media-queries-5") {
-                    $el = $('#media-queries-5 .browser');
-                } else if (document.location.hash == "#/media-queries-5-2") {
-                    $el = $('#media-queries-5-2 .browser');
-                } else if (document.location.hash == "#/media-queries-6") {
-                    $el = $('#media-queries-6 .browser');
-                }
-
-                if ($el.width() > 778) {
-
-                    $el.animate({
-                        width : 778
-                    }, 3000, "linear");
-
-                } else if ($el.width() > 490) {
-
-                    $el.animate({
-                        width : 490
-                    }, 5000, "linear");
-
-                } else if ($el.width() > 330) {
-
-                    $el.animate({
-                        width : 330
-                    }, 3000, "linear");
-
-                } else {
-                    $el.animate({
-                        width : 990
-                    }, 3000, "linear");
-
-                }
+        /**
+         * Loads the demo for the current step into an iframe.
+         */
+        loadDemo : function (step) {
+            if (step.data("demo-loaded")) {
+                return;
             }
-        });
+
+            var browser = step.find(".browser");
+            var addressBar = browser.find(".address div");
+            browser.append("<iframe src='" + addressBar.text() + "' frameborder='0'></iframe>");
+
+            var iframe = browser.find("iframe");
+            iframe.load(function () {
+                addressBar.text(this.src);
+            });
+
+            step.data("demo-loaded", true);
+        },
+
+        /**
+         * Loads the source code for the current step.
+         */
+        loadSrc : function (step) {
+            if (step.data("src-loaded")) {
+                return;
+            }
+
+            $("code[data-src]", step).each(function (index, element) {
+                var $element = $(element);
+                var srcUrl = $element.data("src");
+                var srcLang = $element.data("language");
+                $.get(srcUrl, function (data) {
+                    if (srcLang === "text" || srcLang === "") {
+                        $element.append(data);
+                    } else {
+                        Rainbow.color(data, srcLang, function (highlighted) {
+                            $element.append(highlighted);
+                        });
+                    }
+                }, "text");
+            });
+
+            step.data("src-loaded", true);
+        },
+
+        /**
+         * Attaches a key handlers to resize the browser.
+         */
+        resizeBrowser : function () {
+            $(document).keypress(function (e) {
+                if (e.keyCode === 46) {
+                    var $el;
+                    if (document.location.hash === "#/demo-1") {
+                        $el = $('#demo-1 .browser');
+                    } else if (document.location.hash === "#/demo-2") {
+                        $el = $('#demo-2 .browser');
+                    } else if (document.location.hash === "#/demo-3") {
+                        $el = $('#demo-3 .browser');
+                    } else if (document.location.hash === "#/grid-2") {
+                        $el = $('#grid-2 .browser');
+                    } else if (document.location.hash === "#/grid-4") {
+                        $el = $('#grid-4 .browser');
+                    } else if (document.location.hash === "#/images-3") {
+                        $el = $('#images-3 .browser');
+                    } else if (document.location.hash === "#/media-queries-5") {
+                        $el = $('#media-queries-5 .browser');
+                    } else if (document.location.hash === "#/media-queries-5-2") {
+                        $el = $('#media-queries-5-2 .browser');
+                    } else if (document.location.hash === "#/media-queries-6") {
+                        $el = $('#media-queries-6 .browser');
+                    }
+
+                    if ($el.width() > 778) {
+                        $el.animate({ width : 778 }, 3000, "linear");
+                    } else if ($el.width() > 490) {
+                        $el.animate({ width : 490 }, 5000, "linear");
+                    } else if ($el.width() > 330) {
+                        $el.animate({ width : 330 }, 3000, "linear");
+                    } else {
+                        $el.animate({ width : 990 }, 3000, "linear");
+                    }
+                }
+            });
+        }
     };
 
-    return Presentation;
-}($));
+    $(function () {
+        $(".step").on("impress:stepenter", function () {
+            Responsive.loadSrc($(this));
+            Responsive.loadDemo($(this));
+        });
 
-$(function () {
-    $(".step").on("impress:stepenter", function () {
-        Responsive.loadSrc($(this));
-        Responsive.loadDemo($(this));
+        Responsive.resizeBrowser();
     });
-
-    Responsive.resizeBrowser();
-});
+}());
